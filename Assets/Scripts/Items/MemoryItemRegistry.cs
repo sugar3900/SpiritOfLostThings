36c1a36b237ddcc8799b175d2003de4b95eabd12
Kirsten Sugar <kirsten.sugar@weplaydots.com>
2021-01-29 20:38:38 -0500

    Create Item Registry

---
+++ Assets/Scripts/Items/MemoryItemRegistry.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace GGJ
+{
+	public class MemoryItemRegistry : ScriptableObject
+	{
+		[SerializeField]
+		private GameObject[] memoryItems;
+
+		public GameObject GetMemoryItem(string id)
+		{
+			foreach (GameObject item in memoryItems)
+			{
+				if (item.name == id)
+				{
+					return item;
+				}
+			}
+			return null;
+		}
+	}
+}
