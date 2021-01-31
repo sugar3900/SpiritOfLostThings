f33f981a1584f7f37887ff0d00ab416c47e2edff
Kirsten Sugar <kirsten.sugar@weplaydots.com>
2021-01-31 01:04:00 -0500

    More Items

--- Assets/Scripts/Level/PoemLineData.cs
+++ Assets/Scripts/Level/PoemLineData.cs
@@ -8,9 +8,7 @@ namespace GGJ
 		
 		[SerializeField] public string poemLineContents;
 		[SerializeField] public bool isLight;
-		[SerializeField] public Sprite treeSprite;
 		[SerializeField] public float maxDowseDistance = 5;
 		[SerializeField] public float maxDistanceBeforeTextFades = 5;
-		[SerializeField] public Vector3 position = new Vector3();
 	}
 }
